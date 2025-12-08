95a4f24049e0e4ae64b256940a7ecdb20c784a0a
gspvsr <gspvsr@gmail.com>
2025-12-07 21:45:55 -0600

    i did terraform fmt to failed

--- on_eks/argocd_helm.tf
+++ on_eks/argocd_helm.tf
@@ -5,21 +5,23 @@ resource "helm_release" "argocd" {
   version          = "5.46.0"
   namespace        = "argocd"
   create_namespace = true
-  set {
-    name  = "server.service.type"
-    value = "LoadBalancer"
-  }
 
-  set {
-    name  = "server.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
-    value = "internet-facing"
-  }
+  set = [
+    {
+      name  = "server.service.type"
+      value = "LoadBalancer"
+    },
+    {
+      name  = "server.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
+      value = "internet-facing"
+    },
+    {
+      name  = "server.ingress.enabled"
+      value = "false"
+    }
+  ]
 
-  set {
-    name  = "server.ingress.enabled"
-    value = "false"
-  }
-depends_on = [
+  depends_on = [
     helm_release.aws_load_balancer_controller
   ]
 }
