d608db5ab8d7185e6c252f4aa9422fdea9dc4e8d
gspvsr <gspvsr@gmail.com>
2025-12-07 21:25:51 -0600

    changed helm_karpenter.tf file added set

--- on_eks/eks_argocd_akb_controller.tf
+++ on_eks/eks_argocd_akb_controller.tf
@@ -2,6 +2,13 @@ locals {
   lb_controller_sub = "${replace(data.aws_eks_cluster.eks-cluster.identity[0].oidc[0].issuer, "https://", "")}:sub"
 }
 
+# IAM Policy
+resource "aws_iam_policy" "lb-controller-policy" {
+  name   = "AWSLoadBalancerControllerIAMPolicy"
+  policy = file("iam_policy.json")
+}
+
+# IAM Role
 resource "aws_iam_role" "lb_controller_role" {
   name = "AWSLoadBalancerControllerRole"
 
@@ -22,20 +29,20 @@ resource "aws_iam_role" "lb_controller_role" {
   })
 }
 
-
-
+# Attach Policy to Role
 resource "aws_iam_role_policy_attachment" "alb_attach" {
   role       = aws_iam_role.lb_controller_role.name
   policy_arn = aws_iam_policy.lb-controller-policy.arn
 }
 
+# Kubernetes Namespace
 resource "kubernetes_namespace" "aws_lb_ns" {
   metadata {
     name = "aws-loadbalancer-controller"
   }
 }
 
-# Create the service account
+# Service Account
 resource "kubernetes_service_account" "lb_controller" {
   metadata {
     name      = "aws-load-balancer-controller"
