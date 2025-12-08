b0335c7e72708ada702482fe70ec13c90958e676
gspvsr <gspvsr@gmail.com>
2025-12-07 23:10:52 -0600

    changed helm_karpenter.tf file added set

--- on_eks/helm_lb_controller.tf
+++ on_eks/helm_lb_controller.tf
@@ -6,26 +6,11 @@ resource "helm_release" "aws_load_balancer_controller" {
   version    = "1.7.1"
 
   set = [
-    {
-      name  = "clusterName"
-      value = var.cluster_name
-    },
-    {
-      name  = "region"
-      value = var.aws-region
-    },
-    {
-      name  = "vpcId"
-      value = data.aws_vpc.vpc.id
-    },
-    {
-      name  = "serviceAccount.create"
-      value = "false"
-    },
-    {
-      name  = "serviceAccount.name"
-      value = "aws-load-balancer-controller"
-    }
+    { name = "clusterName"       , value = var.cluster_name },
+    { name = "region"            , value = var.aws_region },
+    { name = "vpcId"             , value = data.aws_vpc.vpc.id },
+    { name = "serviceAccount.create", value = "false" },
+    { name = "serviceAccount.name", value = "aws-load-balancer-controller" }
   ]
 
   depends_on = [
