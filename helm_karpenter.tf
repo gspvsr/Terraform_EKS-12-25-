3a8f8970cb713531a0650e56524b13fa93b05f24
gspvsr <gspvsr@gmail.com>
2025-12-07 20:45:30 -0600

    changed helm_karpenter.tf file added set

--- helm_karpenter.tf
+++ helm_karpenter.tf
@@ -1,24 +1,22 @@
 resource "helm_release" "karpenter" {
   name       = "karpenter"
   namespace  = "karpenter"
-  repository = "oci://public.ecr.aws/karpenter/karpenter"
+  repository = "https://charts.karpenter.sh"
   chart      = "karpenter"
-  version    = "v0.36.1"
+  version    = "1.0.0"
 
-  create_namespace = true
-
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.karpenter_controller.arn
-  }
-
-  set {
-    name  = "settings.clusterName"
-    value = var.cluster_name
-  }
-
-  set {
-    name  = "settings.clusterEndpoint"
-    value = data.aws_eks_cluster.eks.endpoint
-  }
+  set = [
+    {
+      name  = "clusterName"
+      value = var.cluster_name
+    },
+    {
+      name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+      value = aws_iam_role.karpenter_irsa.arn
+    },
+    {
+      name  = "controller.clusterEndpoint"
+      value = data.aws_eks_cluster.eks.endpoint
+    }
+  ]
 }
