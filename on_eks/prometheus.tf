15614f058001352e627a8be051edc7a7ce8ef95f
gspvsr <gspvsr@gmail.com>
2025-12-07 21:47:10 -0600

    changed helm_karpenter.tf file added set

--- on_eks/prometheus.tf
+++ on_eks/prometheus.tf
@@ -1,43 +1,27 @@
-resource "helm_release" "prometheus-helm" {
-  name             = "prometheus"
-  repository       = "https://prometheus-community.github.io/helm-charts"
-  chart            = "kube-prometheus-stack"
-  version          = "65.0.0"
-  namespace        = "prometheus"
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "5.46.0"
+  namespace        = "argocd"
   create_namespace = true
 
-  timeout = 2000
+  set = [
+    {
+      name  = "server.service.type"
+      value = "LoadBalancer"
+    },
+    {
+      name  = "server.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
+      value = "internet-facing"
+    },
+    {
+      name  = "server.ingress.enabled"
+      value = "false"
+    }
+  ]
 
-  set {
-    name  = "podSecurityPolicy.enabled"
-    value = true
-  }
-
-  set {
-    name  = "server.persistentVolume.enabled"
-    value = true
-  }
-
-  set {
-    name  = "grafana.service.type"
-    value = "LoadBalancer"
-  }
-
-  set {
-    name  = "grafana.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
-    value = "internet-facing"
-  }
-
-  set {
-    name  = "prometheus.service.type"
-    value = "LoadBalancer"
-  }
-
-  set {
-  name  = "prometheus.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
-  value = "internet-facing"
-}
-
-
-  depends_on = [helm_release.argocd]
+  depends_on = [
+    helm_release.aws_load_balancer_controller
+  ]
 }
