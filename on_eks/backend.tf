39e0217355cb101c60b88becd38bdc3bd894f8d2
gspvsr <gspvsr@gmail.com>
2025-12-07 21:13:51 -0600

    changed helm_karpenter.tf file added set

--- on_eks/backend.tf
+++ on_eks/backend.tf
@@ -42,7 +42,7 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = data.aws_eks_cluster.eks-cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.eks-cluster-auth.token
